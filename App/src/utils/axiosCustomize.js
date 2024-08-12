a50d52f48abc8262c967b5888ecbe779ff8aeb90
Md0302fe <minhduc2003kols@gmail.com>
2024-08-12 22:24:49 +0700

    Days5/Create a new function update user :>)))

--- App/src/utils/axiosCustomize.js
+++ App/src/utils/axiosCustomize.js
@@ -26,7 +26,7 @@ instance.interceptors.response.use(
     // Do something with response data
     return response && response.data ? response.data : response;
   },
-  
+
   function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
